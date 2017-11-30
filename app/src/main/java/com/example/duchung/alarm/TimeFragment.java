be375a91b9f3f8ad0a79fd7cacfc23719e1a8800
duongduchung <hung13011997@gmail.com>
2017-11-30 12:58:11 +0700

    first

---
+++ app/src/main/java/com/example/duchung/alarm/TimeFragment.java
@@ -0,0 +1,11 @@
+package com.example.duchung.alarm;
+
+/**
+ * Created by dande on 11/28/2017.
+ */
+
+public class TimeFragment extends MyFragment{
+    public TimeFragment(){
+        setName("Đếm ngược");
+    }
+}
